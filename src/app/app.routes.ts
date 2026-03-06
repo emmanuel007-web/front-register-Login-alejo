81770775462aba5d0a74bd55b6a98a9a1df19f4b
emmanuel <emmanuel@192.168.1.21>
2026-03-05 19:39:09 -0500

    First commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { FormRegister } from './components/form-register/form-register';
+
+export const routes: Routes = [
+
+    {path: '', component: FormRegister}
+];
---
+++ src/app/components/form-register/form-register.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-register',
+  imports: [],
+  templateUrl: './form-register.html',
+  styleUrl: './form-register.css',
+})
+export class FormRegister {}
